4cb2c70366d6b3a0dce16b7dc304d076a7882430
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-12-03 02:04:51 +0200

    do not mind this

--- src/Transformation/gsearch.py
+++ src/Transformation/gsearch.py
@@ -1,13 +1,13 @@
+import os
 from src.Transformation.common.kw_extractors import YAKEKeywordExtractor
-from src.Transformation.common.text_clustering import BartLargeMnli
+from src.Transformation.common.text_clustering import LazyFuzzyClassifier
 from src.Transformation.common.spark_udfs import create_keyword_extractor_udf, create_clean_country_udf, create_text_classefier_udf
 from global_variables import LANDING_DIR, LUKES_DATASET_NAME, CLEAN_LOCATION_DATASET_NAME
 
-import os
 gsearch_file_path = os.path.join(LANDING_DIR, LUKES_DATASET_NAME)
 countries_file_path = os.path.join(LANDING_DIR, CLEAN_LOCATION_DATASET_NAME)
 
-txt_classefier = BartLargeMnli()
+txt_classefier = LazyFuzzyClassifier()
 kw_extractor = YAKEKeywordExtractor()
 
 import pandas as pd
@@ -20,58 +20,47 @@ country_names = countries_df["country"].dropna().astype(str).tolist()
 
 del countries_df
 
+
+
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import collect_list, col, explode
+from pyspark.sql.functions import collect_list, col, flatten, explode, desc
+
 
 spark = SparkSession.builder.appName("kw_extractor").getOrCreate()
 
 
-try:
-    kw_extractor_bc         = spark.sparkContext.broadcast(kw_extractor)
-    txt_classefier_bc       = spark.sparkContext.broadcast(txt_classefier)
-    iso2_bc                 = spark.sparkContext.broadcast(iso2)
-    iso3_bc                 = spark.sparkContext.broadcast(iso3)
-    country_names_bc        = spark.sparkContext.broadcast(country_names)
-    candidate_lables_bc     = spark.sparkContext.broadcast(["Data Engineer", "Data Analyst", "Business Analyst", "BI Developer", "Data Scientist"])
-    
-    keyword_extractor_udf   = create_keyword_extractor_udf(kw_extractor_bc)    
-    text_classefier_udf     = create_text_classefier_udf(txt_classefier_bc, candidate_lables_bc)
-    clean_country_udf       = create_clean_country_udf(iso2_list_bc=iso2_bc, iso3_list_bc=iso3_bc, country_names_list_bc=country_names_bc)
+kw_extractor_bc         = spark.sparkContext.broadcast(kw_extractor)
+txt_classefier_bc       = spark.sparkContext.broadcast(txt_classefier)
+iso2_bc                 = spark.sparkContext.broadcast(iso2)
+iso3_bc                 = spark.sparkContext.broadcast(iso3)
+country_names_bc        = spark.sparkContext.broadcast(country_names)
+candidate_lables_bc     = spark.sparkContext.broadcast(["Data Engineer", "Data Analyst", "Business Analyst", "BI Developer", "Data Scientist"])
 
-    df = spark.read.option("header",True).csv(gsearch_file_path)
-    
-    enriched = df.select(
-                df.title
-                , df.location
-                , df.description
-            ).limit(50)\
-            .filter(
-                df.title.isNotNull()
-                & df.location.isNotNull()
-                & df.description.isNotNull()
-            ).withColumn(
-                "country", clean_country_udf(df.location)
-            ).withColumn(
-                "kw_arr", keyword_extractor_udf(df.description)
-            ).filter(
-                col("Country").isNotNull()
-            ).withColumn(
-                "cleaned_job_title", text_classefier_udf(df.title)
-            )\
-            .groupBy(
-                col("cleaned_job_title"), 
-                col("Country")
-            ).agg(
-                collect_list(col("kw_arr")).alias("keywords") 
-            ).withColumn(
-                "keyword", explode(col("keywords"))
-            ).drop("keywords")  
-            
-    enriched.show()
-    # reduced.show()
-except:
-    raise
+keyword_extractor_udf   = create_keyword_extractor_udf(kw_extractor_bc)    
+text_classefier_udf     = create_text_classefier_udf(txt_classefier_bc, candidate_lables_bc)
+clean_country_udf       = create_clean_country_udf(iso2_list_bc=iso2_bc, iso3_list_bc=iso3_bc, country_names_list_bc=country_names_bc)
 
-finally:
-    spark.stop()
-    pass
+
+
+
+df = spark.read.option("header",True).csv(gsearch_file_path)
+
+
+
+enriched = (
+    df.select(col("title"), col("location"), col("description")).filter(
+        col("location").isNotNull() & col("description").isNotNull()
+    )
+    .limit(100)
+    .withColumn("country", clean_country_udf(col("location")))
+    .withColumn("kw_arr", keyword_extractor_udf(col("description")))
+    # .withColumn("clean_title", text_classefier_udf(col("title")))
+    # .filter(col("country").isNotNull())
+    # .drop("location")
+    # .groupBy(col("clean_title"), col("country"))
+    # .agg(collect_list(col("kw_arr")).alias("kw_arr"))
+    # .withColumn("keywords", flatten(col("kw_arr")))
+    # .drop("kw_arr")
+)
+
+enriched.show(truncate=False)
